ee7d23256106536e72dd223b2620548b212fd8cd
frake17 <foxtan757@yahoo.com.sg>
2021-07-27 15:27:45 +0800

    register styling - Kin

--- static/js/optional_question.js
+++
@@ -1,9 +0,0 @@
-Example
-function myFunction() {
-  var x = document.getElementById("optional_question");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
